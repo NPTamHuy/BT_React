52a57613de2e668c4a1b80ba4d8ea9c8f8f1afd3
TamHuy <nptamhuy93@gmail.com>
2026-05-20 23:18:04 +0700

    update App

--- Section 2/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section 2/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -9,7 +16,10 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
   );

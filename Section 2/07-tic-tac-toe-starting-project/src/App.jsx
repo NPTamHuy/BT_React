7e8f931cf1590e04e176c9d7058c0c5a226b0ed0
TamHuy <nptamhuy93@gmail.com>
2026-05-23 09:40:42 +0700

    update App + GameBoard

--- Section 2/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section 2/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,28 +1,56 @@
 import { useState } from 'react';
+
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 import Log from './components/Log';
 
+import { WINNING_COMBINATIONS } from './winning-combinations';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
 
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol;
+    const secondSquareSymbol;
+    const thirdSquareSymbol;
+  }
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
-
-    setGameTurns((prevGameTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevGameTurns.length > 0 && prevGameTurns[0] === 'X') {
-        currentPlayer = 'O';
-      }
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         {
           square: { row: rowIndex, col: colIndex },
           player: currentPlayer,
         },
 
-        ...prevGameTurns,
+        ...prevTurns,
       ];
 
       return updatedTurns;
@@ -44,11 +72,10 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
-        <Log />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   );
 }
-
 export default App;

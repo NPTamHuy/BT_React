ece84641692c91ff4356c38ca7956ff98dca759e
TamHuy <nptamhuy93@gmail.com>
2026-05-23 09:41:04 +0700

    update Log + add winning combination

--- Section 2/07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ Section 2/07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,3 +1,11 @@
-export default function Log() {
-  return <h2>Game Log</h2>;
+export default function Log({ turns }) {
+  return (
+    <ol id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row},{turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 }

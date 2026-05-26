4dba4c79ebbaa105f124e02c16f35198b713722c
TamHuy <nptamhuy93@gmail.com>
2026-05-26 12:27:31 +0700

    update

--- Section 2/07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ Section 2/07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,7 +1,10 @@
 export default function Log({ turns }) {
   return (
     <ol id="log">
+      {/* 1. Gọi hàm .map() để duyệt mảng */}
       {turns.map((turn) => (
+        // 2. Tạo Key độc nhất bằng Template Literals (Dấu Backtick)
+        // Ví dụ: Nếu row=0, col=1 => Key sẽ là chuỗi "01"
         <li key={`${turn.square.row}${turn.square.col}`}>
           {turn.player} selected {turn.square.row},{turn.square.col}
         </li>
